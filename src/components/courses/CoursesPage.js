2189c2436c797d022e6c3e2674aa5016d2991dc5
Oleg Plevan <oleg.plevan@gmail.com>
2024-03-01 12:45:59 +0000

    set up Redux infrostructure

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React from 'react';
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: '',
+    },
+  };
+
+  handleChange = (e) => {
+    const course = { ...this.state.course, [e.target.name]: e.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.course.title);
+  };
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input type='text' name='title' onChange={this.handleChange} value={this.state.course.title} />
+        <input type='submit' value='Save' />
+      </form>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux';
 import App from './components/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('app')
 );
